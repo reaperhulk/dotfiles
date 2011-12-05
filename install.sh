b31b27cae460752a5427bfde68b4fc740f23771f
Paul Kehrer <paul.l.kehrer@gmail.com>
2011-12-04 20:24:34 -0600

    added gemrc/irbrc and updated install script

--- install.sh
+++ install.sh
@@ -6,21 +6,29 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     cd $(dirname "$0")
     CWD=`pwd`
+    #bash files
     ln -sf $CWD/.aliases ~/.aliases
     ln -sf $CWD/.bash_profile ~/.bash_profile
     ln -sf $CWD/.bash_prompt ~/.bash_prompt
     ln -sf $CWD/.bashrc ~/.bashrc
     ln -sf $CWD/.exports ~/.exports
+    ln -sf $CWD/.inputrc ~/.inputrc
     ln -sf $CWD/.functions ~/.functions
+    #lesspipe (used with LESSOPEN in bash exports)
+    rm -rf ~/.dotfiles-bin
+    ln -sf $CWD/.dotfiles-bin ~/.dotfiles-bin
+    #git specific
     ln -sf $CWD/.gitattributes ~/.gitattributes
     ln -sf $CWD/.gitconfig ~/.gitconfig
     ln -sf $CWD/.gitignore ~/.gitignore
+    #vim specific
     ln -sf $CWD/.gvimrc ~/.gvimrc
-    ln -sf $CWD/.inputrc ~/.inputrc
     rm -rf ~/.vim
     ln -sf $CWD/.vim ~/.vim
     ln -sf $CWD/.vimrc ~/.vimrc
-    ln -sf $CWD/.dotfiles-bin ~/.dotfiles-bin
     mkdir -p ~/.vim-tmp
+    #ruby specific
+    ln -sf $CWD/.gemrc ~/.gemrc
+    ln -sf $CWD/.irbrc ~/.irbrc
     source "$HOME/.bash_profile"
 fi
